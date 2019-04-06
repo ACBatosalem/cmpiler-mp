b51a3a28b57dbac4a91448cad97f6d4adcf375f8
louisecortez <louise_cortez@dlsu.edu.ph>
2019-04-06 17:24:34 +0800

    implemented syntax error messages

--- grammar/pascalErrorListener.js
+++ grammar/pascalErrorListener.js
@@ -16,11 +16,25 @@ PascalErrorListener.prototype = Object.create(ErrorListener.ErrorListener.protot
 PascalErrorListener.prototype.constructor = PascalErrorListener;
 
 PascalErrorListener.prototype.syntaxError = function(recognizer, offendingSymbol, line, column, msg, e) { 
+    this.syntaxErrorCheck = true;
+    
+    if(offendingSymbol == null)
+        this.symbol = recognizer.getTokenErrorDisplay(offendingSymbol);
+    else
+        this.symbol = offendingSymbol.text;
+    
     if(msg.includes("mismatched")) { // Missing closing parenthesis
-        process.stdout.write("ERROR at [COLUMN " + column + "] " + msg + "\n");
+        process.stdout.write("SYNTAX ERROR at [COLUMN " + column + "] Mismatched input: " + msg + "\n");
+    } else if (msg.includes("missing")) {
+        process.stdout.write(" SYNTAX ERROR at [COLUMN " + column + "] Missing syntax: " + msg + "\n");
+    } else if (msg.includes("expecting")) {
+        process.stdout.write(" SYNTAX ERROR at [COLUMN " + column + "] Expecting syntax: " + msg + "\n");
+    } else if(msg.includes("token recognition error")) {
+        process.stdout.write("ERROR at COLUMN [" + column + "] Token recognition error: '" + msg.charAt(29) + "' is not a recognized token" + "\n");
     } else {
-        console.log("----- \nrecognizer: " + recognizer + "\noffendingSymbol: " 
-   + offendingSymbol + "\nline: " + line + "\ncol: " + column + "\nmsg: " + msg + "\ne: " + e + "\n-----");   
+        process.stdout.write("ERROR at COLUMN [" + column + "]: " + msg + "\n");
+        // console.log("----- \nrecognizer: " + recognizer + "\noffendingSymbol: " 
+        // + offendingSymbol + "\nline: " + line + "\ncol: " + column + "\nmsg: " + msg + "\ne: " + e + "\n-----");   
     } 
 };
 
